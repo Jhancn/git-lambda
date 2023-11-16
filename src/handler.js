87acc90a29a6c6313ccd374db5e45cad60dc0a93
Jhancn <jhancn39@gmail.com>
2023-11-16 12:32:09 +0530

    added api

--- src/handler.js
+++ src/handler.js
@@ -3,5 +3,11 @@
 module.exports.generateRandomNumber = async (event) => {
   const randomNumber = parseInt(Math.random()*100);
   console.log("Random generated Number is",randomNumber);
-  return randomNumber;
+  
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ number: randomNumber }),};
  };
